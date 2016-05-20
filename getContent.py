86cbc9f06903cfcd797d1883d83940db84e25e62
hyanx <huiyana@foxmail.com>
2016-05-20 23:12:45 +0800

    增加获取转发微博的正则表达式

--- getContent.py
+++ getContent.py
@@ -10,8 +10,10 @@ def getContent(StrPage):
     cook = {"Cookie": ""}
     url = 'http://weibo.cn/xxxxxxxxxx/profile?page=' + StrPage		# 其中x为UID账号
     html = requests.get(url, cookies = cook)
-    reOrigin = re.compile(ur'<div class="c" id="M_(\w+)"><div><span class="ctt">(.*?)</span>.*?cmtfrm" class="cc">评论\[(\d)\]</a>.*?class="ct">(\d{2}月\d{2}日) ([\d:]{4,10})&nbsp;来自(.*?)</span>')
-    content = re.findall(reOrigin,html.text)
+    regexOrigin = re.compile(ur'<div class="c" id="M_(\w{9})"><div><span class="ctt">(.*?)</span>.*?cmtfrm" class="cc">评论\[(\d{1,2})\]</a>.*?class="ct">(\d{2}月\d{2}日|[\d-]{10}) ([\d:]{5,8})&nbsp;来自(.*?)</span>')
+    regexPost = re.compile(ur'<div class="c" id="M_(\w{9})"><div><span class="cmt">转发了&nbsp;<a href="(.*?)</a>.*?class="ctt">(.*?)</span>&nbsp;.*?class="cmt">转发理由:</span>(.*?)&nbsp;&nbsp;<a href="http://weibo.cn/attitude.*?class="cc">评论\[(\d{1,2})\]</a>.*?class="ct">(\d\d月\d\d日|[\d-]{10}) ([\d:]{5,8})&nbsp;来自(.*?)</span></div></div>')
+
+    content = re.findall(regexOrigin,html.text)
     textToken = ''
     for i in range(len(content)):
         text = ''
